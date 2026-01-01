822e63b79fbe7e1fb9e2f46a017173d7c270d1c9
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-01-01 21:25:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from time import sleep
 from collections import defaultdict
 import sqlite3
 from datetime import datetime, timedelta
+import os
 
 
 def connect_db():
@@ -455,6 +456,7 @@ atexit.register(lambda: scheduler.shutdown())
 
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  
+    app.run(host='0.0.0.0', port=port, debug=True)
 
-    app.run(debug=True)
 
